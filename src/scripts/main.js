3f42f51f0080d632db98fc245cd82e66793fa1e5
Mykhailo Forkosh <forkos277@gmail.com>
2024-12-14 06:11:50 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const htmlTable = [...document.querySelector('table').rows];
+
+htmlTable.forEach((row) => {
+  if (row.childElementCount > 1) {
+    const cloneElement = row.children[1].cloneNode(true);
+
+    row.lastElementChild.before(cloneElement);
+  }
+});
